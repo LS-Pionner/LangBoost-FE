4314accde94ca898c155ae7f5d261fb5be97928d
Ms-You <msu122005@hs.ac.kr>
2025-02-27 17:35:06 +0900

    refactor: 공용 문장 세트 컴포넌트 분리

---
+++ src/components/features/public/PublicSentenceSetComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="sentenceset-item">
+        <div class="sentenceset-title">
+            <h3>{{ truncatedTitle }}</h3>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+    sentenceSet: {
+        type: Object,
+        required: true
+    },
+});
+
+const truncatedTitle = computed(() => {
+    const title = props.sentenceSet.title;
+
+    return title.length > 15 ? title.substring(0, 15) + '...' : title;
+});
+
+</script>
+
+<style scoped>
+.sentenceset-item {
+  position: relative;
+  padding: 15px;
+  margin-bottom: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+}
+
+.sentenceset-item:hover {
+  background-color: #ececec;
+}
+
+.sentenceset-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.sentenceset-item h3 {
+  margin: 0;
+  font-size: 18px;
+  color: #333;
+}
+
+</style>
--- src/components/features/public/PublicSentenceSetListComponent.vue
+++ src/components/features/public/PublicSentenceSetListComponent.vue
@@ -1,26 +1,11 @@
 <template>
   <div class="scrollable-container" @scroll="handleScroll">
     <div class="sentenceset-container">
-      <div
+      <PublicSentenceSetComponent
         v-for="sentenceSet in sentenceSetList"
         :key="sentenceSet.id"
-        class="sentenceset-item"
-        @click="toggleMeaning(sentenceSet.id)"
-      >
-        <div class="sentenceset-header">
-          <h3>{{ sentenceSet.title }}</h3>
-          <button
-            class="details-button"
-            @click.stop="navigateToSentenceSet(sentenceSet.id)"
-          >
-            세부
-          </button>
-        </div>
-        <!-- <p v-if="visibleMeaningId === sentenceSet.id" class="meaning">
-          {{ sentenceSet.description }}
-        </p>
-        <p class="last-viewed">최근 조회일: {{ sentenceSet.lastViewedAt }}</p> -->
-      </div>
+        :sentenceSet="sentenceSet"
+      />
       <div v-if="loading" class="loading">Loading...</div>
     </div>
   </div>
@@ -29,13 +14,13 @@
 <script setup>
 import axios from '@/axios';
 import { ref, onMounted } from 'vue';
+import PublicSentenceSetComponent from './PublicSentenceSetComponent.vue';
 
 const sentenceSetList = ref([]);  // API에서 가져온 문장 목록
 const offset = ref(0);  // param으로 전달할 offset (데이터를 가져올 시작 위치)
 const limit = 10; // param으로 전달할 limit (한 번에 가져올 데이터 수)
 const loading = ref(false); // 데이터 로딩 상태
 const isLastPage = ref(false);  // 마지막 페이지 여부
-const visibleMeaningId = ref(null); // 의미를 보여줄 문장의 ID (초기 값은 null)
 
 onMounted(() => {
   // 컴포넌트가 마운트될 때 초기 데이터 로드
@@ -84,16 +69,11 @@ const handleScroll = (event) => {
   }
 }
 
-// 문장 세트를 클릭했을 때 토글로 설명을 보여주는 함수
-const toggleMeaning = (id) => {
-  visibleMeaningId.value = visibleMeaningId.value === id ? null : id;
-}
-
 // 문장 세트를 클릭하면 문장 세트 상세 보기로 이동하는 함수
-const navigateToSentenceSet = (id) => {
-  const frontendBaseURL = process.env.VUE_APP_FRONTEND_BASE_URL;
-  window.location.href = `${frontendBaseURL}/public/sentence-set/${id}`; // 문자열 보간법을 사용하여 URL 설정
-}
+// const navigateToSentenceSet = (id) => {
+//   const frontendBaseURL = process.env.VUE_APP_FRONTEND_BASE_URL;
+//   window.location.href = `${frontendBaseURL}/public/sentence-set/${id}`; // 문자열 보간법을 사용하여 URL 설정
+// }
 
 </script>
 
@@ -111,58 +91,6 @@ const navigateToSentenceSet = (id) => {
 .sentenceset-container {
   max-width: 600px;
   margin: 0 auto;
-}
-
-.sentenceset-item {
-  position: relative;
-  padding: 15px;
-  margin-bottom: 20px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  background-color: #f9f9f9;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-}
-
-.sentenceset-item:hover {
-  background-color: #ececec;
-}
-
-.sentenceset-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.details-button {
-  font-size: 12px;
-  padding: 5px 10px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 3px;
-  cursor: pointer;
-}
-
-.details-button:hover {
-  background-color: #0056b3;
-}
-
-.sentenceset-item h3 {
-  margin: 0;
-  font-size: 18px;
-  color: #333;
-}
-
-.meaning {
-  margin: 10px 0;
-  font-size: 14px;
-  color: #555;
-}
-
-.last-viewed {
-  font-size: 12px;
-  color: #999;
 }
 
 .loading {
